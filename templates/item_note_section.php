7c945656ed30b9a13968ee2ca4a024f50848e7f9
Peter de Souza <peterdesouza@gmail.com>
2017-03-26 10:50:55 +0100

    added item template for note section

---
+++ templates/item_note_section.php
@@ -0,0 +1,29 @@
+<div class="col-md-8">
+
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Title of section<span class="glyphicon glyphicon-trash pull-right delete-note" aria-hidden="true"></span></h3>
+
+		</div>
+		<div class="panel-body">
+
+			<div class="row">
+				<div class="col-md-6">
+					Anatomy notes to go here...
+				</div>
+
+				<div class="col-md-6">
+					<ul class="list-group">
+						<li class="list-group-item">Action 1</li>
+						<li class="list-group-item">Action 2</li>
+						<li class="list-group-item">Action 3</li>
+						<li class="list-group-item">Action 4</li>
+						<li class="list-group-item">Action 5</li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+</div>
