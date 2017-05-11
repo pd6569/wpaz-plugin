23c59f72074acfdb80febd710382ae02b6530384
Peter de Souza <peterdesouza@gmail.com>
2017-05-11 22:56:20 +0100

    using webpack and es6 modules

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/**
+ * Created by peter on 11/05/2017.
+ */
+
+const path = require('path');
+
+module.exports = {
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve('./js'),
+                loader:'babel-loader',
+            }
+        ],
+
+
+    },
+
+    entry: path.resolve('./js/3dnotes.js'),
+        output: {
+        filename: './dist/bundle.js',
+    },
+
+    devtool: 'source-map'
+
+}
