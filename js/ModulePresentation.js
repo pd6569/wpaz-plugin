c09691dc2831607ea847838d125f3b81ece94809
Peter de Souza <peterdesouza@gmail.com>
2017-05-15 11:24:14 +0100

    fullscreen for 3d widget working

--- js/ModulePresentation.js
+++ js/ModulePresentation.js
@@ -16,8 +16,9 @@ export default class ModulePresentation extends BaseModule {
     enableModule () {
         console.log(this.moduleName + " enabled");
         appGlobals.mode.PRESENTATION = true;
-        this.setPresentationOverlay();
         this.setListeners();
+
+        this.setPresentationOverlay();
     }
 
     disableModule () {
@@ -38,17 +39,38 @@ export default class ModulePresentation extends BaseModule {
 
         jQuery('body').append(this.$presentationOverlay);
 
+        this.setWidgetFullScreen();
+        this.$presentationOverlay.append(this.app.$iframeContainer);
     }
 
     removePresentationOverlay(){
+        this.app.$humanWidget.width("100%");
+        this.app.$humanWidget.height(600);
+        this.app.$modelContainer.prepend(this.app.$iframeContainer);
         this.$presentationOverlay.remove();
     }
 
     setListeners(){
         console.log("setListeners");
+
         jQuery(document).keyup((event) => {
+            console.log("disable presentation module");
             if (event.keyCode === 27) this.disableModule();
+        });
+
+        jQuery(window).off();
+        jQuery(window).on('resize', (event) =>{
+            this.setWidgetFullScreen();
         })
     }
+
+    setWidgetFullScreen(){
+        let viewportHeight = jQuery(window).height();
+        let viewPortWidth = jQuery(window).width();
+
+        this.app.$humanWidget.width(viewPortWidth);
+        this.app.$humanWidget.height(viewportHeight);
+    }
+
 
 }
