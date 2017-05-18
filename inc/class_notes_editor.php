e3dbb166b1834a36064d0074b08fd2b7bd9bfbfd
Peter de Souza <peterdesouza@gmail.com>
2017-05-18 09:02:16 +0100

    removed tinymce add scene/action plugin

--- inc/class_notes_editor.php
+++ inc/class_notes_editor.php
@@ -14,8 +14,6 @@ class wpaz_notes_editor {
 
 	private function setup_editor(){
 		add_filter('mce_css', array($this, 'plugin_mce_css'));
-		add_filter('mce_external_plugins', array($this, 'add_tinymce_plugin'));
-		add_filter('mce_buttons', array($this, 'add_tinymce_button'));
 	}
 
 	public function plugin_mce_css($mce_css) {
@@ -30,13 +28,4 @@ class wpaz_notes_editor {
 
 	}
 
-	public function add_tinymce_plugin($plugin_array){
-		$plugin_array['linkscene'] = WP_AZ_PLUGIN_URL . 'assets/tinymce-wpaz-plugins/tinymce_buttons.js';
-		return $plugin_array;
-	}
-
-	public function add_tinymce_button($buttons){
-		array_push($buttons, 'linkscene');
-		return $buttons;
-	}
 }
